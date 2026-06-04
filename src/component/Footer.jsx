3beb097675e08abcd387990f3117787093838682
saad7528 <saad0174742@gmail.com>
2026-06-04 22:08:16 +0600

    add number

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => {
           <div>
             <h3 className="text-white mb-3 tracking-wide">CONTACT US</h3>
             <ul className="space-y-2">
-              <li>786 901 1622</li>
+              <li>+8801851192657</li>
               <li>info@wandarland.com</li>
             </ul>
           </div>
