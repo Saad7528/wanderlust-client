db0584c07eebe984750dd5b9e95866143954ed22
saad7528 <saad0174742@gmail.com>
2026-06-08 11:30:57 +0600

    create destinations page

---
+++ src/app/destinations/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const destinationsPage = () => {
+    return (
+        <div>
+            <h1>Explore All Destinations</h1>
+        </div>
+    );
+};
+
+export default destinationsPage;
